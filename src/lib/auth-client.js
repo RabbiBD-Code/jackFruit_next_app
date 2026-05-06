acb8cb010205dd01a5317ebb06e5abeaed5d3d23
RabbiBD-Code <mdrabbi.cse@gmail.com>
2026-05-06 23:39:45 +0600

    add all ok

--- src/lib/auth-client.js
+++ src/lib/auth-client.js
@@ -2,5 +2,5 @@
 import { createAuthClient } from "better-auth/react"
 export const authClient = createAuthClient({
     /** The base URL of the server (optional if you're using the same domain) */
-    baseURL: "https://jack-fruit-next-app.vercel.app"
+    baseURL: "http://localhost:3000"
 })
